7a55cf017b699b22cef228340421724ff25b0506
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2022-03-23 00:21:34 +0300

    Добавьте файлы проекта.

---
+++ WpfLab19/Models/Line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLab19.Models
+{
+    static class Line
+    {
+       public static double Add(int a)=>2*Math.PI*a;
+    }
+}
---
+++ WpfLab19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfLab19.Models;
+
+namespace WpfLab19.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private int radius;
+        public int Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double result;
+        public double Result
+        {
+            get => result;
+            set
+            {
+                result = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+
+        private void OnAddCommandExecute(object p)
+        {
+            Result = Line.Add(radius);
+        }
+
+        private bool CanAddCommandExecuted(object p)
+        {
+            return true;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+
+    }
+}
